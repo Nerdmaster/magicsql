4a9a13c6ed8da234d80c48ec66e798fa908cda6e
Nerdmaster <brody76@gmail.com>
2017-09-26 18:23:36 -0700

    Add time save/reload cycle validation test

--- db_time_test.go
+++ db_time_test.go
@@ -53,4 +53,15 @@ func TestSaveTime(t *testing.T) {
 		"fooTimeList[0].CreatedAt was stored and retrieved correctly", t)
 	assert.Equal(up.UTC().String(), fooTimeList[0].UpdatedAt.UTC().String(),
 		"fooTimeList[0].UpdatedAt was stored and retrieved correctly", t)
+
+	// Are times being preserved and re-read properly?
+	var ft1, ft2 []*FooTime
+	op.Save("foo_times", fooTimeList[0])
+	op.Select("foo_times", &FooTime{}).AllObjects(&ft1)
+	op.Save("foo_times", ft1[0])
+	op.Select("foo_times", &FooTime{}).AllObjects(&ft2)
+	assert.Equal(time.Unix(1473000000, 0).UTC().String(), ft2[0].CreatedAt.UTC().String(),
+		"After saving and reloading multiple times, created_at is still correct", t)
+	assert.Equal(time.Unix(1474000000, 0).UTC().String(), ft2[0].UpdatedAt.UTC().String(),
+		"After saving and reloading multiple times, updated_at is still correct", t)
 }
